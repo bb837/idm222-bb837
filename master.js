0b781a45478528c51f0ad6d6e96313e82f19de3e
bb837 <43794585+bb837@users.noreply.github.com>
2019-02-21 02:57:10 -0500

    update to content

--- master.js
+++ master.js
@@ -1,16 +1,33 @@
 //   Splash    //
-
+function closeSplash(){
+  document.getElementById("fade-out").style.visibility = "collapse";
+}
+document.getElementById("splash-closebtn").addEventListener("click",closeSplash, false);
 
 //    NAV      //
 
-/* Open */
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-  }
-  
-  /* Close */
-  function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-  }
+function openNav(){
+  document.getElementById("myNav").style.height = "100%";
+}
 
-  console.log(myNav)
+document.getElementById("icon").addEventListener("click", openNav, false);
+
+function closeNav(){
+  document.getElementById("myNav").style.height = "0%";
+}
+document.getElementById("closebtn").addEventListener("click",closeNav, false);
+
+
+
+
+
+// /* Open */
+// function openNav() {
+//     document.getElementById("myNav").style.height = "100%";
+//   }
+  
+// /* Close */
+//   function closeNav() {
+//     document.getElementById("myNav").style.height = "0%";
+//   }
+//   console.log(myNav)
