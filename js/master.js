b25b2fec0349c64681149be37297c8b126acb70d
bb837 <bb837@drexel.edu>
2019-03-07 18:47:26 -0500

    update to content
    bug fixed

--- js/master.js
+++ js/master.js
@@ -1,12 +1,7 @@
-//   Splash    //
-function closeSplash(){
-  document.getElementById("fade-out").style.visibility = "collapse";
-}
-document.getElementById("splash-closebtn").addEventListener("click",closeSplash, false);
-
 //    NAV      //
 
 function openNav(){
+  console.log('open');
   document.getElementById("myNav").style.height = "100%";
 }
 
@@ -14,6 +9,7 @@ document.getElementById("icon").addEventListener("click", openNav, false);
 
 function closeNav(){
   document.getElementById("myNav").style.height = "0%";
+
 }
 document.getElementById("closebtn").addEventListener("click",closeNav, false);
 
---
+++ js/splash.js
@@ -0,0 +1,5 @@
+//   Splash    //
+function closeSplash(){
+    document.getElementById("fade-out").style.visibility = "collapse";
+  }
+  document.getElementById("splash-closebtn").addEventListener("click",closeSplash, false);
