f46702d0499ad7164d56bfcb6b228a30bd8f6d4b
bb837 <bb837@drexel.edu>
2019-03-08 01:23:00 -0500

    update to content
    splash.js made (bug fix)
    promise added
    local storage added
    
    header added

--- js/splash.js
+++ js/splash.js
@@ -3,3 +3,30 @@ function closeSplash(){
     document.getElementById("fade-out").style.visibility = "collapse";
   }
   document.getElementById("splash-closebtn").addEventListener("click",closeSplash, false);
+
+// PROMISE
+const introElement = document.getElementById('fade-out');
+const promiseEvents = new Promise(function(resolve, reject) {
+  if (introElement.classList.contains('fade-out')) {
+    window.setTimeout(function() {
+      resolve();
+    }, 6000);
+  }
+});
+
+promiseEvents.then(function() {
+  closeSplash();
+})
+
+// //  LOCAL STORAGE /////
+
+const introWatched = localStorage.getItem('introWatched');
+if (introWatched === 'true') 
+{
+  introElement.setAttribute.visibility = "none";
+  console.log('Intro Played');
+}
+
+localStorage.setItem('introWatched','true');
+
+console.log(introWatched);
